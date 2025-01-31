5f988766f7ea7f96f93e4ada1598b79c6ae78f49
Kyle Johnsen <kjohnsen@uga.edu>
2025-01-30 22:29:23 -0500

    adds a canvas for smooth display, and adds a no connection indication

--- ResourceMonitor/resource_monitor.py
+++ ResourceMonitor/resource_monitor.py
@@ -16,41 +16,50 @@ else:
     print("No serial ports found")
     exit()
 
-m5stick = serial.Serial(port.name,115200)
-m5stick.read_all() # read all the garbage at the start
-cpu = 0
-last_read_time = time.time()
+m5stick = serial.Serial(port.name,115200) # creates and opens the serial port
 
-event_log = pd.DataFrame({"timestamp":[],"state":[]})
-while m5stick.is_open:
-    try:
-        if m5stick.in_waiting > 0:
-            # read the byte
-            event_code = m5stick.read()
-            event_log.loc[-1] = [datetime.datetime.now().timestamp(),event_code]
-            print("Got ", event_code, "from M5 Stick")
+last_read_time = time.time() # we are keeping track of the last time we read/sent data, so we can do it regularly
+event_log = [] # this will hold tuples of data representing time,value events
+while m5stick.is_open: # Loop while we have an open serial connection
+    try: # we need to handle serial exceptions, e.g., unplugged
+        if m5stick.in_waiting > 0: #there is data
+            # read a byte at a time
+            event_code = m5stick.read(1)
 
-        curr_time = time.time()
-        if curr_time - last_read_time < 1: continue
+            # If the event code is the same as a newline, it's probably a garbage message
+            # The esp sends these at startupm, but they all end in \n, so we can detect them
+            if event_code[0] == ord('\n'):
+                event_log = [] # we drop the log if we get this message, because the esp restarted
+                continue
+            if event_code[0] == 0xFF:
+                break # we are done
+            else:
+                event_log.append((datetime.datetime.now().timestamp(),event_code[0]))
 
-        last_read_time = curr_time
+        # we check if it's time to do something
+        curr_time = time.time() 
+        if curr_time - last_read_time < 1: continue # we won't do anything until 1s has passed
+        
+        last_read_time = curr_time #remember the last time we read data
+
+        # now read the data from psutil (cpu, memory, battery)
         cpu = int(psutil.cpu_percent())
         mem = psutil.virtual_memory()
         mem = int(mem.available / mem.total *100)
-        battery = 0
+        battery = 0 
+        # not every pc has a battery 
         if hasattr(psutil, "sensors_battery"):
             battery = psutil.sensors_battery()
             if battery:
                 battery = int(battery.percent)
+
         m5stick.write(bytearray([cpu,mem,battery])) 
         
     except Exception as e:
         print(e)
         break
 
-
-
 m5stick.close()
-
+event_log = pd.DataFrame(event_log,columns=["timestamp","value"])
 event_log.to_csv(f'event_log_{datetime.datetime.now().timestamp()}.csv',index=False)
 
--- m5resource/m5resource.ino
+++ m5resource/m5resource.ino
@@ -1,10 +1,16 @@
 #include "M5Unified.h"
+#include "M5GFX.h"
+
+unsigned long last_message = 0;
+M5Canvas canvas(&M5.Display); // a canvas allows you to do all drawing off-screen, helping to avoid artifacts from drawing while displaying
 void setup() {
-  // put your setup code here, to run once:
   M5.begin();
   Serial.begin(115200);
+  M5.Display.setRotation(1);
+  canvas.createSprite(M5.Display.width(),M5.Display.height()); //we only want to create this once, after the rotation, so it's correct
+  canvas.setTextSize(2);
+  last_message = millis();
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
   M5.update();
@@ -13,9 +19,10 @@ void loop() {
     int cpu = Serial.read();
     int mem = Serial.read();
     int bat = Serial.read();
-    //M5.Display.clear();
-    M5.Display.setCursor(0,0);
-    M5.Display.printf("CPU=%d,MEM=%d,BAT=%d",cpu,mem,bat);
+    canvas.clear();
+    canvas.setCursor(0,10);
+    canvas.printf(" CPU=%d\n MEM=%d\n BAT=%d",cpu,mem,bat);
+    last_message = millis();
   }
   if(M5.BtnA.wasPressed()){
     Serial.write(1);
@@ -23,5 +30,15 @@ void loop() {
   if(M5.BtnA.wasReleased()){
     Serial.write(0);
   }
-  M5.delay(1);
+  if(M5.BtnB.wasPressed()){
+    Serial.write(255);
+  }
+
+  unsigned long curr_time = millis();
+  if(curr_time-last_message > 2000){
+    canvas.clear();
+    canvas.setCursor(5,10);
+    canvas.printf("No connection");
+  }
+  canvas.pushSprite(0,0); //this assumes the canvas is the same size as the screen
 }
